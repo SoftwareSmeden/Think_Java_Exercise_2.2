6c66d9b78f0336c31aa0c3de2d0f74fcffbb26e5
SoftwareSmeden <torb815g@edu.easj.dk>
2021-09-03 19:18:33 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Think Java - Exercise 2.2
+
+        var day = "Friday";
+        int date = 03;
+        var month = "September";
+        int year = 2021;
+
+        System.out.println("American format: " + day + " " + month + " " + date + "," + " " + year );
+        System.out.println("European format: " + day + " " + date + " " + month + " " + year );
+    }
+}
