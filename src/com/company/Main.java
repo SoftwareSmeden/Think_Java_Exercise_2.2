eb5b6f0b0572c5c23f0b122c21d94f33fa3b022c
SoftwareSmeden <torb815g@edu.easj.dk>
2021-09-07 12:30:35 +0200

    Update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// Think Java - Exercise 2.2
 
-        var day = "Friday";
+        String day = "Friday";
         int date = 03;
-        var month = "September";
+        String month = "September";
         int year = 2021;
 
         System.out.println("American format: " + day + " " + month + " " + date + "," + " " + year );
